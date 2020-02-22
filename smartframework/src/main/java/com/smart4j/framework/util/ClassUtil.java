fadc0dcb52a3760df2c6373b3b378b83346f98fd
yuqiu-pp <41912446@qq.com>
2020-02-23 00:12:15 +0800

    classUtil

--- smartframework/src/main/java/com/smart4j/framework/util/ClassUtil.java
+++ smartframework/src/main/java/com/smart4j/framework/util/ClassUtil.java
@@ -3,8 +3,16 @@ package com.smart4j.framework.util;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
+import java.io.FileFilter;
+import java.io.IOException;
+import java.net.JarURLConnection;
+import java.net.URL;
+import java.util.Enumeration;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 
 /**
  * 类操作工具类
@@ -49,7 +57,82 @@ public final class ClassUtil {
      */
     public static Set<Class<?>> getClassSet(String packageName){
         Set<Class<?>> classSet = new HashSet<Class<?>>();
+        try {
+            Enumeration<URL> urls = getClassLoader().getResources(packageName.replace(".", "/"));
+            while (urls.hasMoreElements()){
+                URL url = urls.nextElement();
+                if (url != null){
+                    String protocol = url.getProtocol();
+                    if (protocol.equals("file")){
+                        String packagePath = url.getPath().replaceAll("%20", " ");
+                        addClass(classSet, packagePath, packageName);
+                    }else if(protocol.equals("jar")){
+                        addJarClass(classSet, url);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            LOGGER.error("get class set failure", e);
+            throw new RuntimeException(e);
+        }
 
         return classSet;
+    }
+
+    private static void addJarClass(Set<Class<?>> classSet, URL ul) throws IOException {
+        JarURLConnection jarURLConnection = (JarURLConnection)ul.openConnection();
+            if (jarURLConnection != null){
+                JarFile jarFile = jarURLConnection.getJarFile();
+                if (jarFile != null){
+                    Enumeration<JarEntry> jarEntryEnumeration = jarFile.entries();
+                    while (jarEntryEnumeration.hasMoreElements()){
+                        JarEntry jarEntry = jarEntryEnumeration.nextElement();
+                        String jarEntryName = jarEntry.getName();
+                        if (jarEntryName.endsWith(".class")){
+                            String className = jarEntryName.substring(0, jarEntryName.lastIndexOf("."))
+                                    .replaceAll("/", ".");
+                            doAddClass(classSet, className);
+                        }
+                    }
+                }
+            }
+    }
+
+    private static void addClass(Set<Class<?>> classSet, String packagePath, String packageName) {
+        File[] files = new File(packagePath).listFiles(new FileFilter() {
+            @Override
+            public boolean accept(File file) {
+                return (file.isFile() && file.getName().endsWith(".class")) || file.isDirectory();
+            }
+        });
+
+        for (File file : files) {
+            String fileName = file.getName();
+            if (file.isFile()) {
+                String className = fileName.substring(0, fileName.indexOf("."));
+                if (StringUtil.isEmpty(packageName)) {
+                    className = packageName + "." + className;
+                }
+                doAddClass(classSet, className);
+            } else {
+                String subPackagePath = fileName;
+                if (StringUtil.isEmpty(packagePath)) {
+                    subPackagePath = packagePath + "/" + subPackagePath;
+                }
+                String subPackageName = fileName;
+                if (StringUtil.isEmpty(packagePath)) {
+                    subPackageName = packagePath + "." + subPackageName;
+                    {
+                    }
+                    addClass(classSet, subPackagePath, subPackageName);
+                }
+            }
+        }
+    }
+
+
+    private static void doAddClass(Set<Class<?>> classSet, String className){
+        Class<?> cls = loadClass(className, false);
+        classSet.add(cls);
     }
 }
